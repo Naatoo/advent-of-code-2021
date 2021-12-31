692dd69106c023a6f22d3aa9b5bc1eac33b710bd
naatoo <thenatooorat@gmail.com>
2021-12-31 04:04:58 +0100

    add day24

---
+++ src/day24/Solution.java
@@ -0,0 +1,17 @@
+package day24;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        ArithmeticLogicUnit aLU = new ArithmeticLogicUnit();
+        // Unfortunately did it on paper again
+        System.out.println("Part 1: " + 45989929946199L);
+        System.out.println("Part 2: " + 11912814611156L);
+    }
+
+}
+
+class ArithmeticLogicUnit {
+
+}
